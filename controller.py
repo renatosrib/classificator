93bfb137aa1f7f7584b1056d7186364c7ced66b9
renatosrib <renatosrib@gmail.com>
2016-09-27 00:22:26 -0300

    Evolução no cadastro

--- controller.py
+++ controller.py
@@ -1,13 +1,18 @@
-from flask import Flask
+from flask import Flask, request, current_app
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Sistema IMDB!'
 
+@app.route('/form')
+def cadastro_categoria():
+    return current_app.send_static_file('form.html')
+
 @app.route('/cadastrar', methods=['POST'])
 def cadastro():
-    return 'registrado com sucesso'
+    return str(request.form['category']) + 'registrado com sucesso'
 
 @app.route('/listar')
 def listar():
